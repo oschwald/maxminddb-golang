a0ae122750dc4c85bc4b629c43d10c0c12ca73ae
Gregory Oschwald <goschwald@maxmind.com>
2015-02-23 19:18:11 -0800

    Do not use unsafe on Google App Engine. Fixes #10

--- decoder.go
+++ decoder.go
@@ -7,7 +7,6 @@ import (
 	"math/big"
 	"reflect"
 	"sync"
-	"unsafe"
 )
 
 type decoder struct {
@@ -507,26 +506,6 @@ func (d *decoder) decodeKeyString(offset uint) (string, uint, error) {
 		return "", 0, fmt.Errorf("unexpected type when decoding string: %v", typeNum)
 	}
 	return d.decodeString(size, newOffset)
-}
-
-// decodeStructKey returns a string which points into the database. Don't keep
-// it around.
-func (d *decoder) decodeStructKey(offset uint) (string, uint, error) {
-	typeNum, size, newOffset := d.decodeCtrlData(offset)
-	switch typeNum {
-	case _Pointer:
-		pointer, ptrOffset := d.decodePointer(size, newOffset)
-		s, _, err := d.decodeStructKey(pointer)
-		return s, ptrOffset, err
-	case _String:
-		var s string
-		val := (*reflect.StringHeader)(unsafe.Pointer(&s))
-		val.Data = uintptr(unsafe.Pointer(&d.buffer[newOffset]))
-		val.Len = int(size)
-		return s, newOffset + size, nil
-	default:
-		return "", 0, fmt.Errorf("unexpected type when decoding structkey: %v", typeNum)
-	}
 }
 
 // This function is used to skip ahead to the next value without decoding
