22a8a3aa073afb10629263bafcd4d15e39dd5cc7
Gregory Oschwald <goschwald@maxmind.com>
2015-03-28 19:45:02 -0700

    Refactor network iterator API
    The idea is to make it more similar to the database/sql Rows
    iterator that people are already familiar with and to allow for
    easier use in a for loop.
    
    I also added an example test to show how to use it.

--- traverse_test.go
+++ traverse_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestTraverse(t *testing.T) {
+func TestNetworks(t *testing.T) {
 	for _, recordSize := range []uint{24, 28, 32} {
 		for _, ipVersion := range []uint{4, 6} {
 			fileName := fmt.Sprintf("test-data/test-data/MaxMind-DB-test-ipv%d-%d.mmdb", ipVersion, recordSize)
@@ -15,22 +15,22 @@ func TestTraverse(t *testing.T) {
 			}
 			defer reader.Close()
 
-			i := reader.Traverse()
-
-			for {
-				var recordInterface interface{}
-				network, err := i.Next(&recordInterface)
+			n := reader.Networks()
+			for n.Next() {
+				record := struct {
+					IP string `maxminddb:"ip"`
+				}{}
+				network, err := n.Network(&record)
 				if err != nil {
 					t.Fatal(err)
-				} else if network == nil {
-					break
 				}
 
-				record := recordInterface.(map[string]interface{})
-
-				if record["ip"] != network.IP.String() {
-					t.Fatalf("expected %s got %s", record["ip"], network.IP.String())
+				if record.IP != network.IP.String() {
+					t.Fatalf("expected %s got %s", record.IP, network.IP.String())
 				}
+			}
+			if n.Err() != nil {
+				t.Fatal(n.Err())
 			}
 		}
 	}
