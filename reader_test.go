5e019ecfda8cb1b71893f4507a9e77dbeb2c56d7
Paul H. Müller <paul@adjust.com>
2016-09-14 11:53:08 +0200

    fix benchmark paths

--- reader_test.go
+++ reader_test.go
@@ -484,7 +484,7 @@ func checkIpv6(t *testing.T, reader *Reader) {
 }
 
 func BenchmarkMaxMindDB(b *testing.B) {
-	db, err := Open("GeoLite2-City.mmdb")
+	db, err := Open("test-data/test-data/GeoIP2-City-Test.mmdb")
 	assert.Nil(b, err)
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -499,7 +499,7 @@ func BenchmarkMaxMindDB(b *testing.B) {
 }
 
 func BenchmarkCountryCode(b *testing.B) {
-	db, err := Open("GeoLite2-City.mmdb")
+	db, err := Open("test-data/test-data/GeoIP2-City-Test.mmdb")
 	assert.Nil(b, err)
 
 	type MinCountry struct {
