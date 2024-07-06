c1486399e327c05bc7e9b479480397ec7de44fd9
Gregory Oschwald <goschwald@maxmind.com>
2024-07-06 16:21:42 -0700

    Add the ability to decode a single path
    This is more ergonomic than creating a struct for a single value and
    the performance is better as well due to less reflection:
    
    BenchmarkDecodeCountryCodeWithStruct-8   	 1347441	       882.4 ns/op	       1 B/op	       0 allocs/op
    BenchmarkDecodePathCountryCode-8         	 2708011	       445.1 ns/op	       1 B/op	       0 allocs/op

--- result.go
+++ result.go
@@ -46,6 +46,51 @@ func (r Result) Decode(v any) error {
 	return err
 }
 
+// DecodePath unmarshals a value from data section into v, following the
+// specified path.
+//
+// The v parameter should be a pointer to the value where the decoded data
+// will be stored. If v is nil or not a pointer, an error is returned. If the
+// data in the database record cannot be stored in v because of type
+// differences, an UnmarshalTypeError is returned.
+//
+// The path is a variadic list of keys (strings) and/or indices (ints) that
+// describe the nested structure to traverse in the data to reach the desired
+// value.
+//
+// For maps, string path elements are used as keys.
+// For arrays, int path elements are used as indices.
+//
+// If the path is empty, the entire data structure is decoded into v.
+//
+// Returns an error if:
+//   - the path is invalid
+//   - the data cannot be decoded into the type of v
+//   - v is not a pointer or the database record cannot be stored in v due to
+//     type mismatch
+//   - the Result does not contain valid data
+//
+// Example usage:
+//
+//	var city string
+//	err := result.DecodePath(&city, "location", "city", "names", "en")
+//
+//	var geonameID int
+//	err := result.DecodePath(&geonameID, "subdivisions", 0, "geoname_id")
+func (r Result) DecodePath(v any, path ...any) error {
+	if r.err != nil {
+		return r.err
+	}
+	if r.offset == notFound {
+		return nil
+	}
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return errors.New("result param must be a pointer")
+	}
+	return r.decoder.decodePath(r.offset, path, rv)
+}
+
 // Err provides a way to check whether there was an error during the lookup
 // without clling Result.Decode. If there was an error, it will also be
 // returned from Result.Decode.
